bb093627ba32856013be5c6a1bdcca40d7c8f6f0
Ieatdirtdirt <jaysinghcool1@gmail.com>
2025-09-28 16:57:48 -0400

    I did some tuning btw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -176,7 +176,7 @@ class LocalizationTest extends OpMode {
  * @author Baron Henderson - 20077 The Indubitables
  * @version 1.0, 5/6/2024
  */
-class ForwardTuner extends OpMode {
+class       ForwardTuner extends OpMode {
     public static double DISTANCE = 48;
 
     @Override
